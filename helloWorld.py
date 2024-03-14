5849b97af1ef59669952bec9528a43a4196be70f
Gavra-S <158857092+Gavra-S@users.noreply.github.com>
2024-03-14 15:56:47 +0000

    Update helloWorld.py to include comments in code

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,8 @@
+# Into message to user
 print("Git is awesome!")
+# Variable for storing user name
 user_name = input("What is your name?: ")
+# Variable for storing user age
 user_age = input("How old are you?: ")
+# Prints out greeting followed by user name and age
 print("Hello " + user_name! + "You are " + user_age + " years old.")
